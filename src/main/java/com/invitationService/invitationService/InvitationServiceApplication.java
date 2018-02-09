5680f283072cfb76ae394c97dc85e82981668a6b
Ramin Manesh <ramin.safarmanesh>
2018-02-09 12:22:35 +0100

    project created

---
+++ src/main/java/com/invitationService/invitationService/InvitationServiceApplication.java
@@ -0,0 +1,12 @@
+package com.invitationService.invitationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvitationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvitationServiceApplication.class, args);
+	}
+}
