254988cff1a4275b2ece520ea6b8f05f7f1738c1
Jannik Meier <jannik.meier@cgi.com>
2018-02-13 14:08:24 +0100

    port and color changes

--- src/main/java/com/invitationService/invitationService/InvitationServiceController.java
+++ src/main/java/com/invitationService/invitationService/InvitationServiceController.java
@@ -23,14 +23,20 @@ public class InvitationServiceController {
 		List<TableRow> liste = new ArrayList<>();
 		liste.add(new TableRow("Peter", "e@mail.de", true));
 		liste.add(new TableRow("Peter", "e@mail.de", false));
-		liste.add(new TableRow("Peter", "e@mail.de", true));
+		liste.add(new TableRow("Christian", "e@mail.de", true));
 		liste.add(new TableRow("Hans", "e@mail.de", true));
 		liste.add(new TableRow("Peter", "e@mail.de", true));
-		liste.add(new TableRow("Peter", "e@mail.de", true));
-		liste.add(new TableRow("Peter", "e@mail.de", true));
-		liste.add(new TableRow("Peter", "e@mail.de", true));
-		liste.add(new TableRow("Peter", "e@mail.de", true));
-		liste.add(new TableRow("Peter", "e@mail.de", true));
+		liste.add(new TableRow("Anna", "e@mail.de", true));
+		liste.add(new TableRow("Lukas", "e@mail.de", true));
+		liste.add(new TableRow("Simone", "e@mail.de", true));
+		liste.add(new TableRow("Angela", "e@mail.de", true));
+		liste.add(new TableRow("Bernd", "e@mail.de", false));
+		liste.add(new TableRow("Chantal", "e@mail.de", true));
+		liste.add(new TableRow("Dummy", "e@mail.de", true));
+		liste.add(new TableRow("Simple", "e@mail.de", true));
+		liste.add(new TableRow("Questionaire", "e@mail.de", true));
+		liste.add(new TableRow("CGI", "e@mail.de", true));
+		liste.add(new TableRow("Consulting", "e@mail.de", true));
 		model.addAttribute("liste", liste);
 		return "index";
 	}
