336946f09f4f59f96f704a5a62c604a13bce3f20
Jannik Meier <jannik.meier@cgi.com>
2018-02-13 17:46:04 +0100

    loginform

--- src/main/java/com/invitationService/invitationService/InvitationServiceController.java
+++ src/main/java/com/invitationService/invitationService/InvitationServiceController.java
@@ -4,17 +4,62 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.core.type.filter.RegexPatternTypeFilter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.invitationService.models.TableRow;
 
 @Controller
 @EnableAutoConfiguration
 public class InvitationServiceController {
+
+	
+
+	
+	@GetMapping("/login")
+	public String login(Model model) {
+		
+		model.addAttribute("user", new TableRow());
+		return "login_form";
+	}
+	
+	
+	@PostMapping("/goToDesigner")
+	public String goToDesigner(@ModelAttribute @Validated TableRow user, RedirectAttributes redirectAttributes) {
+		
+		String usermail = user.getEmail();
+		
+		//TODO Validate Email
+		
+//		checkEmail()
+		
+		if(usermail.length()<1 ) {
+
+			redirectAttributes.addFlashAttribute("errormessage", "Kein gültiges Emailformat!");
+			return"redirect:/login";
+			
+		}
+		else if(true){
+			
+			
+			redirectAttributes.addFlashAttribute("errormessage", "Kein gültiges Emailformat!");
+			return"redirect:/login";
+		}
+		else {
+			return "designerMock";
+		}
+		
+	}
+	
 
 	
 	
--- src/main/java/com/invitationService/models/TableRow.java
+++ src/main/java/com/invitationService/models/TableRow.java
@@ -1,14 +1,21 @@
 package com.invitationService.models;
 
+import javax.validation.constraints.Size;
+
+import org.springframework.validation.annotation.Validated;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class TableRow {
 
+	
 	private String Name;
 	private String Email;
 	private Boolean Status;
