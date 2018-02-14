f0641a46e6d376e813b80c539ec68b39950b4ef7
Unknown <j.schipper1@hotmail.de>
2018-02-14 17:17:56 +0100

    fix for error

--- src/main/java/com/invitationService/invitationService/InvitationServiceController.java
+++ src/main/java/com/invitationService/invitationService/InvitationServiceController.java
@@ -7,9 +7,10 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.core.type.filter.RegexPatternTypeFilter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,35 +30,31 @@ import com.invitationService.services.EmailService;
 @EnableAutoConfiguration
 public class InvitationServiceController {
 
-
-	
 	@GetMapping("/login")
 	public String login(Model model) {
-		
+
 		model.addAttribute("user", new User());
 		return "login_form";
 	}
-	
-	
-	@PostMapping("/goToDesigner")
-	public String goToDesigner(@Valid @ModelAttribute User user, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
-				
-		if(bindingResult.hasErrors()) {
-			redirectAttributes.addFlashAttribute("errormessage", "Hoppla, da ist etwas schief gegangen.. \\n hast du eine valide Emailadresse eingegeben?");
-			return "redirect:/login";
-		}else {
-			
-			//TODO redirect to designer service mit user.email als attribute
-			return "designerMock";
-		}			
-	}
-	
 
-	
-	
+	@PostMapping("/goToDesigner")
+	public String goToDesigner(@Valid @ModelAttribute User user, BindingResult bindingResult,
+			RedirectAttributes redirectAttributes) {
+
+		if (bindingResult.hasErrors()) {
+			redirectAttributes.addFlashAttribute("errormessage",
+					"Hoppla, da ist etwas schief gegangen.. \\n hast du eine valide Emailadresse eingegeben?");
+			return "redirect:/login";
+		} else {
+
+			// TODO redirect to designer service mit user.email als attribute
+			return "designerMock";
+		}
+	}
 
 	@Autowired
 	private EmailService emailService;
+
 	@RequestMapping("/")
 	public String home(Model model) {
 		List<TableRow> liste = new ArrayList<>();
@@ -82,22 +79,20 @@ public class InvitationServiceController {
 		return "index";
 	}
 
-	
 	@ResponseBody
-	@RequestMapping(value="/sendInvitationEmails", method=RequestMethod.POST)
-	public User SendInvitationEmails(@RequestBody User user){
+	@RequestMapping(value = "/sendInvitationEmails", method = RequestMethod.POST)
+	public User SendInvitationEmails(@RequestBody User user) {
 		emailService.sendMail(user);
 		return user;
 	}
-	
+
 	@ResponseBody
-	@RequestMapping(value="/sendInvitationEmailsList", method=RequestMethod.POST)
-	public List<User> SendInvitationEmailsList(@RequestBody List<User> users){
-		users.forEach(s->System.out.println(s));
-		//emailService.sendMail(user);
+	@RequestMapping(value = "/sendInvitationEmailsList", method = RequestMethod.POST)
+	public List<User> SendInvitationEmailsList(@RequestBody List<User> users) {
+		users.forEach(s -> System.out.println(s));
+		// emailService.sendMail(user);
 		return null;
 	}
-	
 
 	@RequestMapping("/list/{id}")
 	public List<TableRow> getRows(@PathVariable String id) {
