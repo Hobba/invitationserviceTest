21fc186e7490a2e2a98c077e444422d20e03618b
Unknown <j.schipper1@hotmail.de>
2018-02-16 15:32:55 +0100

    refactor note ...

--- src/main/java/com/invitationService/invitationService/InvitationServiceController.java
+++ src/main/java/com/invitationService/invitationService/InvitationServiceController.java
@@ -28,6 +28,8 @@ import com.invitationService.services.EmailService;
 @Controller
 @EnableAutoConfiguration
 public class InvitationServiceController {
+	
+	//TODO: REFACTOR THIS!
 
 	@GetMapping("/login")
 	public String login(Model model) {
