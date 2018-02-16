8fa0f7dea7d4b47b96c15e9ec8c44fb5b5ccee2f
Patrick Trapp <patrick.trapp@cgi.com>
2018-02-16 22:32:08 +0100

    DEV

--- devStart.sh
+++ devStart.sh
@@ -1,5 +1,5 @@
 JAR_NAME=$1
 kill `cat /home/centos/$JAR_NAME.pid`
 export SPRING_PROFILES_ACTIVE=production
-nohup java -jar /home/centos/$JAR_NAME > /dev/null 2>&1 &
+nohup java  -Dserver.servlet.context-path=/IS -jar /home/centos/$JAR_NAME > /dev/null 2>&1 &
 echo $! > /home/centos/$JAR_NAME.pid
