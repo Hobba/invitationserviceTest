f743f67ad9d0f2325025a54284a6f55df8b711ff
Jan Schipper <j.schipper1@hotmail.de>
2018-02-19 17:21:05 +0100

    fixed imports

--- src/main/java/com/invitationService/invitationService/ProductionConfig.java
+++ src/main/java/com/invitationService/invitationService/ProductionConfig.java
@@ -3,11 +3,8 @@ package com.invitationService.invitationService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.mongodb.MongoDbFactory;
-import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 
 import com.invitationService.services.MailgunEmailService;
-import com.mongodb.MongoClient;
 import com.invitationService.services.EmailService;
 
 @Configuration
