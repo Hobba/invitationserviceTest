ef24ddbcaf751e6bcaf1c5793eff68aa376f140c
VivaceVivo <patrick.trapp@web.de>
2018-02-20 21:17:35 +0100

    nu aber

--- devStart.sh
+++ devStart.sh
@@ -1,5 +1,5 @@
 JAR_NAME=$1
-$TOKEN_KEY=$2
+TOKEN_KEY=$2
 kill `cat /home/centos/$JAR_NAME.pid`
 export SPRING_PROFILES_ACTIVE=production
 nohup java --token.key="$TOKEN_KEY" -Dserver.servlet.context-path=/IS -jar /home/centos/$JAR_NAME > /dev/null 2>&1 &
