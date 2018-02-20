3fc166fa2249829d0a5b1c170d42fc7d109702ef
Unknown <jannik_meier@yahoo.de>
2018-02-20 17:52:50 +0100

    bean of type tokenservice

--- src/main/java/com/invitationService/invitationService/InvitationServiceApplication.java
+++ src/main/java/com/invitationService/invitationService/InvitationServiceApplication.java
@@ -2,11 +2,19 @@ package com.invitationService.invitationService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.invitationService.tokenmaster.TokenService;
 
 @SpringBootApplication
 public class InvitationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InvitationServiceApplication.class, args);
+	}
+	
+	@Bean
+	public TokenService tokenService() {
+		return new TokenService();
 	}
 }
