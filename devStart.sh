6a0798cd1c485162da037e0f4442c5caa1fcb9b3
VivaceVivo <patrick.trapp@web.de>
2018-02-20 21:45:15 +0100

    changed component scan

--- devStart.sh
+++ devStart.sh
@@ -2,5 +2,5 @@ JAR_NAME=$1
 TOKEN_KEY=$2
 kill `cat /home/centos/$JAR_NAME.pid`
 export SPRING_PROFILES_ACTIVE=production
-nohup java --token.key="$TOKEN_KEY" -Dserver.servlet.context-path=/IS -jar /home/centos/$JAR_NAME > /dev/null 2>&1 &
+nohup java -jar /home/centos/$JAR_NAME --token.key="$TOKEN_KEY" > /dev/null 2>&1 &
 echo $! > /home/centos/$JAR_NAME.pid
--- src/main/java/com/invitationService/invitationService/InvitationServiceApplication.java
+++ src/main/java/com/invitationService/invitationService/InvitationServiceApplication.java
@@ -3,9 +3,11 @@ package com.invitationService.invitationService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 import com.invitationService.tokenmaster.TokenService;
 
+@ComponentScan("com.invitationService")
 @SpringBootApplication
 public class InvitationServiceApplication {
 
