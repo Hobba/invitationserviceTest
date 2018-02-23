344a422876b3fcbf4833e50e5679788aa37b9493
Unknown <jannik_meier@yahoo.de>
2018-02-23 15:40:19 +0100

    added token testclass

---
+++ src/test/java/com/invitationService/invitationService/TokenServiceTest.java
@@ -0,0 +1,33 @@
+package com.invitationService.invitationService;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import com.invitationService.tokenmaster.TokenService;
+
+public class TokenServiceTest {
+
+	private TokenService ts;
+	
+	@Before
+	public void init() {
+		ts = new TokenService();
+		ReflectionTestUtils.setField(ts, "key", "secret");
+		
+		
+	}
+	
+	@Test
+	public void tokenParsing() {
+		
+		String testemail = "test@example.de";
+		System.out.println(testemail);
+		String token = ts.createUserJWT("", "IS", "invitation", testemail, "42");
+		System.out.println(token);
+		assertEquals(testemail, ts.parseJWT(token));
+		
+	}
+}
