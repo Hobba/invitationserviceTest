dd18c1e7c8b11a173fb290314d6f03ce1bd010d2
ramin manesh <r.safar.manesh@gmail.com>
2018-02-26 12:45:40 +0100

    creatorDao is reafactored

--- src/main/java/com/invitationService/invitationService/Config.java
+++ src/main/java/com/invitationService/invitationService/Config.java
@@ -2,6 +2,12 @@ package com.invitationService.invitationService;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+
+import com.mongodb.MongoClient;
 
 @Configuration
 public class Config {
@@ -10,6 +16,17 @@ public class Config {
 	public CreatorDAO creatorDAO() {
 		return new CreatorDAO();
 	}
+	
+	@Bean
+	public MongoDbFactory mongoDbFactory() {
+		return new SimpleMongoDbFactory(new MongoClient(), "bootcamp");
+	}
+
+	@Bean
+	public MongoOperations mongoTemplate() {
+		return new MongoTemplate(mongoDbFactory());
+	}
+
 	
 //	@Bean
 //	public ApiKeyClass api() {
