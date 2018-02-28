f71ebd180b0f8c9e8a8c2f22e343f40c1c41b07b
Unknown <j.schipper1@hotmail.de>
2018-02-28 18:40:05 +0100

    impressum

--- src/main/java/com/invitationService/invitationService/InvitationServiceController.java
+++ src/main/java/com/invitationService/invitationService/InvitationServiceController.java
@@ -62,6 +62,11 @@ public class InvitationServiceController {
 			return "login_form";
 		}
 	}
+	
+	@RequestMapping("/impressum")
+	String impressum(Model model) {
+		return "impressum";
+	}
 
 	@Autowired
 	private EmailService emailService;
