ebcbb005d54aade88e8477a5b4e1da0581d7e152
Jannik <jannik_meier@yahoo.de>
2018-03-01 13:06:01 +0100

    testcoverage >20%

--- src/test/java/com/invitationService/invitationService/TestCreatorDAO.java
+++ src/test/java/com/invitationService/invitationService/TestCreatorDAO.java
@@ -8,6 +8,8 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.internal.matchers.Any;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.BasicQuery;
+
 import static org.mockito.Mockito.verify;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.times;
@@ -18,9 +20,12 @@ import static org.springframework.data.mongodb.core.query.Query.query;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.Part;
+
 import static org.mockito.Mockito.never;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.doReturn;
 
 import com.invitationService.models.Creator;
 import com.invitationService.models.Participant;
@@ -35,6 +40,7 @@ public class TestCreatorDAO {
 
 	@Mock
 	private Participant participant;
+	
 
 	@Before
 	public void init() {
@@ -65,12 +71,9 @@ public class TestCreatorDAO {
 	@Test
 	public void testeHasParticipantAnsweredFalse() {
 
-		
-		
 		when(participant.getHasAnswered()).thenReturn(false);
 		when(participant.getSurvey_id()).thenReturn("1");
-		when(participant.getEmail()).thenReturn("test@example.com");
-		when(template.findOne(query(where("email").is(participant.getEmail()).and("id").is(participant.getSurvey_id())), 
+		when(template.findOne(query(where("email").is(participant.getEmail()).and("survey_id").is(participant.getSurvey_id())),
 				Participant.class)).thenReturn(participant);
 
 		Assert.assertEquals(false, creatorDAO.hasParticipantAnswered(participant));
@@ -79,17 +82,105 @@ public class TestCreatorDAO {
 
 	@Test
 	public void testeHasParticipantAnsweredTrue() {
-		
-		
+
 		when(participant.getHasAnswered()).thenReturn(true);
 		when(participant.getSurvey_id()).thenReturn("1");
-		when(participant.getEmail()).thenReturn("test@example.com");
-		when(template.findOne(query(where("email").is(participant.getEmail()).and("id").is(participant.getSurvey_id())), 
-				Participant.class)).thenReturn(participant);
 		
-		
+		 when(template.findOne(query(where("email").is(participant.getEmail()).and("survey_id").is(participant.getSurvey_id())),
+		 Participant.class)).thenReturn(participant);
+
 		Assert.assertEquals(true, creatorDAO.hasParticipantAnswered(participant));
+
+	}
+	
+	
+	@Test
+	public void testeSetParticipantHasAnswered() {
+		Participant parti = new Participant("test@example.com", "token", "1", false);
+		List<Participant> liste = new ArrayList<>();
+		liste.add(parti);
+//		when(participant.getEmail()).thenReturn("test@example.com");
+//		when(participant.getSurvey_id()).thenReturn("1");
+		when(template.find(query(where("email").is("test@example.com").and("survey_id").is("1")),
+					Participant.class)).thenReturn(liste);
 		
+		creatorDAO.setParticipantAsAnswered(parti);
+		
+		Assert.assertEquals(true, parti.getHasAnswered());
+	}
+	
+	@Test
+	public void testeSetParticipantHasAnsweredNoParticipantInDB() {
+		Participant parti = new Participant("test@example.com", "token", "1", false);
+		List<Participant> liste = new ArrayList<>();
+		
+//		when(participant.getEmail()).thenReturn("test@example.com");
+//		when(participant.getSurvey_id()).thenReturn("1");
+		when(template.find(query(where("email").is("test@example.com").and("survey_id").is("1")),
+					Participant.class)).thenReturn(liste);
+		
+		creatorDAO.setParticipantAsAnswered(parti);
+		
+		Assert.assertEquals(false, parti.getHasAnswered());
+	}
+	
+	
+	@Test
+	public void testeIsCreatorExistTrue() {
+		
+		BasicQuery q = new BasicQuery("{ \"email\" : \"" + "test@example.com" + "\"}");
+		List<Creator> liste = new ArrayList<>();
+		Creator creator = new Creator();
+		creator.setEmail("test@example.com");
+		liste.add(creator);
+		System.out.println(liste.size());
+		
+		when(template.find(q, Creator.class)).thenReturn(liste);
+		
+		Assert.assertEquals(true, creatorDAO.isCreatorExist("test@example.com"));
+		
+	}
+	
+	@Test
+	public void testeIsCreatorExistFalse() {
+		
+		BasicQuery q = new BasicQuery("{ \"email\" : \"" + "falsetest@example.com" + "\"}");
+		List<Creator> liste = new ArrayList<>();
+		Creator creator = new Creator();
+		creator.setEmail("test@example.com");
+		liste.add(creator);
+		System.out.println(liste.size());
+		
+		when(template.find(q, Creator.class)).thenReturn(liste);
+		
+		Assert.assertEquals(false, creatorDAO.isCreatorExist("test@example.com"));
+		
+	}
+	
+	@Test
+	public void testeGetAllParticipantsGetNone() {
+		when(participant.getSurvey_id()).thenReturn("1");
+		List<Participant> liste = new ArrayList<>(); 
+		
+		
+		when(template.find(query(where("survey_id").is("1")), Participant.class)).thenReturn(liste);
+		
+		
+		Assert.assertEquals(liste, creatorDAO.getAllParticipantsForSurvey("1"));
+	}
+	
+	@Test
+	public void testeGetAllParticipantsGetSome() {
+		when(participant.getSurvey_id()).thenReturn("1");
+		List<Participant> liste = new ArrayList<>();
+		liste.add(participant);
+		liste.add(participant);
+		liste.add(participant);
+		
+		when(template.find(query(where("survey_id").is("1")), Participant.class)).thenReturn(liste);
+		
+		
+		Assert.assertEquals(liste, creatorDAO.getAllParticipantsForSurvey("1"));
 	}
 
 }
