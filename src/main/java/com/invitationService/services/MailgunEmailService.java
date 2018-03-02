a75e8eac454ce5c7178fd541f75bbc7ecb4411c1
Unknown <j.schipper1@hotmail.de>
2018-03-02 10:28:21 +0100

    better not

--- src/main/java/com/invitationService/services/MailgunEmailService.java
+++ src/main/java/com/invitationService/services/MailgunEmailService.java
@@ -97,8 +97,8 @@ public class MailgunEmailService implements EmailService {
 				email.setContent(email.getContent().replaceAll("\\$\\{TITLE\\}", survey.getTitle()));
 				email.setContent(
 						email.getContent().replaceAll("\\$\\{CREATORNAME\\}", getCreatorName(survey.getCreator())));
-				email.setContent(
-						email.getContent().replaceAll("\\$\\{GREETING\\}", survey.getSettings().getGreeting()));
+//				email.setContent(
+//						email.getContent().replaceAll("\\$\\{GREETING\\}", survey.getSettings().getGreeting()));
 				email.setContent(
 						email.getContent().replaceAll("\\$\\{USERLINK\\}", surveyservice_base_url + "?user=" + token));
 				LOGGER.info("Eine Email für einen Teilnehmer wurde erstellt");
